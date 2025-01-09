2a86ff382ac59bdd9332167bd8b518b79152c9c5
Harsh Gharat <128292084+hhharrrshhh@users.noreply.github.com>
2025-01-09 21:30:09 +0530

    Update routes.py

--- sgd/routes.py
+++ sgd/routes.py
@@ -7,10 +7,10 @@ from datetime import datetime
 
 
 MANIFEST = {
-    "id": "shuvamjaswal.stremio.googledrive",
+    "id": "hhharrrshhh.stremio.googledrive",
     "version": "1.1.0",
     "name": "GDrive",
-    "description": "This plugin fetches content from goolag drive.",
+    "description": "This plugin fetches content from google drive.",
     "logo": "https://fonts.gstatic.com/s/i/productlogos/drive_2020q4/v8/web-512dp/logo_drive_2020q4_color_1x_web_512dp.png",
     "resources": ["stream"],
     "types": ["movie", "series"],
